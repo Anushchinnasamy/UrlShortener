98c37519b1fe45c8df74acd53f294a4172487e1b
Anush <anushchinnasamy@gmail.com>
2026-04-21 09:42:34 +0530

    Adding Service layer

--- src/main/java/com/urlshortener/url_shortnener/Controllers/UrlController.java
+++ src/main/java/com/urlshortener/url_shortnener/Controllers/UrlController.java
@@ -1,14 +1,11 @@
 package com.urlshortener.url_shortnener.Controllers;
 
-import com.urlshortener.url_shortnener.Entity.Url;
-import com.urlshortener.url_shortnener.Repository.UrlRepository;
-import com.urlshortener.url_shortnener.Util.Base62Encoder;
-import jakarta.servlet.http.HttpServletResponse;
+
+
+import com.urlshortener.url_shortnener.Service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -16,45 +13,17 @@ import java.util.Map;
 public class UrlController {
 
     @Autowired
-    private UrlRepository urlRepository;
+    private UrlService urlService;
 
     @PostMapping("/shorten")
     public Map<String, String> shortenUrl(@RequestBody Map<String, String> request) {
-
-        String longUrl = request.get("url");
-
-        // Save first to get ID
-        Url url = new Url();
-        url.setLongUrl(longUrl);
-
-        Url savedUrl = urlRepository.save(url);
-
-        // Encode ID
-        String shortCode = Base62Encoder.encode(savedUrl.getId());
-
-        // Save shortCode
-        savedUrl.setShortCode(shortCode);
-        urlRepository.save(savedUrl);
-
-        String shortUrl = "http://localhost:8080/" + shortCode;
-
-        Map<String, String> response = new HashMap<>();
-        response.put("shortUrl", shortUrl);
-
-        return response;
+        return urlService.createShortUrl(request.get("url"));
     }
 
     @GetMapping("/analytics/{shortCode}")
     public Map<String, Object> getAnalytics(@PathVariable String shortCode) {
-
-        Url url = urlRepository.findByShortCode(shortCode)
-                .orElseThrow(() -> new RuntimeException("URL not found"));
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("shortCode", url.getShortCode());
-        response.put("longUrl", url.getLongUrl());
-        response.put("clickCount", url.getClickCount());
-
-        return response;
+        return urlService.getAnalytics(shortCode);
     }
+
+
 }
